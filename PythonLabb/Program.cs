748f09830955214a6a2c704aa1d6d1b9be47c722
linehanna <caroline.helmvee@magsit.se>
2025-02-21 09:46:25 +0100

    Update Program.cs

--- PythonLabb/Program.cs
+++ PythonLabb/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿IGNORE THIS FILE PLEASE
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
